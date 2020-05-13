323c5bdbd529e1b3532891f2ff9da8ad00791b27
juger rash <jugerrash@jugers-Mac.local>
2020-05-12 17:32:50 -0700

    Setup the project

---
+++ FullApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FullApp
+//
+//  Created by juger rash on 12.05.20.
+//  Copyright © 2020 juger rash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
